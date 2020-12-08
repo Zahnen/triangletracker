d0395f81fc9d8595f285e58df048c249fe8a74ad
Zahnen Garner <zahnen@gmail.com>
2020-12-08 11:32:14 -0800

    fail third test
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -23,9 +23,10 @@ namespace TriangleTracker
       {
         return false;
       }
-    public bool isIsosceles(int side1, int side2, int side3)
+    }
+    public bool IsIsosceles(int side1, int side2, int side3)
     {
-      if(side1 == side2 || side2 == side3 || side1 = side3)
+      if((side1 == side2 && side1 != side3) || (side2 == side3 && side2 != side1) || (side1 == side3 && side3 != side2))
       {
         return true;
       }
